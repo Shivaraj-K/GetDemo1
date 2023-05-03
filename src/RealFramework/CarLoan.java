891ac7cd089647f9440f16e4829f8ef987b06c04
Shivaraj-K <kshivu741@gmail.com>
2023-05-03 16:43:15 +0530

    First commit

---
+++ src/RealFramework/CarLoan.java
@@ -0,0 +1,24 @@
+package RealFramework;
+
+import org.testng.annotations.Test;
+
+public class CarLoan {
+
+	@Test
+	public void loginCar()
+	{
+		System.out.println("loggedCar");
+	}
+	
+	@Test
+	public void carName()
+	{
+		System.out.println("Innova");
+	}
+	@Test
+	public void Brand()
+	{
+		System.out.println("Lamber");
+	}
+
+}
---
+++ src/RealFramework/PersonalLoan.java
@@ -0,0 +1,22 @@
+package RealFramework;
+
+import org.testng.annotations.Test;
+
+public class PersonalLoan {
+
+	@Test
+	public void studentLoan()
+	{
+		System.out.println("Student Loan");
+	}
+	@Test
+	public void employeeLoan()
+	{
+		System.out.println("employee Loan");
+	}
+	@Test
+	public void LandLoan()
+	{
+		System.out.println("land loan");
+	}
+}
